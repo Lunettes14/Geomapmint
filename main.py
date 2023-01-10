75709e2d395c459dadbbac21945275a0f232d0bb
Lunettes14 <48411588+Lunettes14@users.noreply.github.com>
2023-01-11 00:30:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ path = r'/home/aina/PycharmProjects/geomapmint/s2_21/ROIs2017_winter_s2_21_p'
 def _read_dataset(size, path, begin):
     for j in range(0, 29 * int(math.sqrt(size)), 29):
         for i in range(begin, begin + int(math.sqrt(size))):
-            if not os.path.exists(path + f'{i+j}.tif'):
+            if not os.path.exists(path + f'{i + j}.tif'):
                 return False
     return True
 
@@ -23,11 +23,11 @@ def _read_dataset(size, path, begin):
 def _is_compatible(size, path, begin):
     if _read_dataset(size, path, begin):
         for j in range(0, 29 * int(math.sqrt(size)), 29):
-                for i in range(begin+1, begin + int(math.sqrt(size))):
-                    if not _are_horizontal_neighbours(i+j-1, i+j):
+                for i in range(begin + 1, begin + int(math.sqrt(size))):
+                    if not _are_horizontal_neighbours(i + j - 1, i + j):
                         return False
-        for j in range(int(math.sqrt(size))-1):
-            if not _are_vertical_neighbours(begin + 29*j, begin + 29*(j+1)):
+        for j in range(int(math.sqrt(size)) - 1):
+            if not _are_vertical_neighbours(begin + 29 * j, begin + 29 * (j + 1)):
                 return False
         return True
     else:
@@ -52,8 +52,3 @@ def _are_horizontal_neighbours(left, right):
         return False
 
 print(_is_compatible(169, path, 30))
-
-
-
-
-
